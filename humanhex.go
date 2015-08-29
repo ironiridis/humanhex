09b1e9614258465be7ee75cd6d1670157ca36e73
Chris Harrington <ironiridis@gmail.com>
2015-08-29 11:14:59 -0500

    implement with tests

--- humanhex.go
+++ humanhex.go
@@ -1,5 +1,11 @@
 package humanhex
 
+import (
+	"bytes"
+	"fmt"
+)
+
+// eww, this is bad and i feel bad
 func readable(b byte) bool {
 	switch {
 	case b < 0x20:
@@ -11,10 +17,36 @@ func readable(b byte) bool {
 	}
 }
 
+// less eww
+func lookahead(b []byte, p int) int {
+	var i = p
+	for ; i < len(b); i++ {
+		if !readable(b[i]) {
+			break
+		}
+	}
+	return i - p
+}
+
 // String will return a more human-consumable representation of the bytes in b.
 // m specifies the number of contiguous human-friendly bytes required before we
-// will display it directly. 2 is a good default.
-func String(b []byte, m uint) string {
-
-	return ""
+// will display it directly. 2 is a good default. 0 is undefined and panics.
+// Note that human-consumable and human-friendly here are limited in scope to
+// North American, English-speaking humans. Maybe Go's rune type can do some
+// more heavy lifting on our behalf down the road?
+func String(b []byte, m int) string {
+	if m == 0 {
+		panic("invalid value 0 for m")
+	}
+	out := &bytes.Buffer{}
+	for i := 0; i < len(b); {
+		if contig := lookahead(b, i); contig >= m {
+			out.WriteString(string(b[i : i+contig]))
+			i += contig
+		} else {
+			out.WriteString(fmt.Sprintf("\\x%02x", b[i]))
+			i++
+		}
+	}
+	return out.String()
 }
---
+++ humanhex_test.go
@@ -0,0 +1,80 @@
+package humanhex
+
+import "testing"
+
+type testcase struct {
+	try    []byte
+	contig int
+	want   string
+}
+
+func z(t *testing.T, in []testcase) {
+	for _, tc := range in {
+		got := String(tc.try, tc.contig)
+		if got != tc.want {
+			t.Errorf("String(%q, %v) == %q, wanted %q", tc.try, tc.contig, got, tc.want)
+		}
+	}
+}
+
+func TestAllNulls(t *testing.T) {
+	z(t, []testcase{
+		{[]byte{0, 0, 0, 0}, 1, "\\x00\\x00\\x00\\x00"},
+		{[]byte{0, 0, 0, 0, 0}, 1, "\\x00\\x00\\x00\\x00\\x00"},
+		{[]byte{0, 0, 0, 0}, 2, "\\x00\\x00\\x00\\x00"},
+		{[]byte{0, 0, 0, 0, 0}, 2, "\\x00\\x00\\x00\\x00\\x00"},
+		{[]byte{0, 0, 0, 0}, 10, "\\x00\\x00\\x00\\x00"},
+		{[]byte{0, 0, 0, 0, 0}, 10, "\\x00\\x00\\x00\\x00\\x00"},
+	})
+}
+
+func TestStartReadable(t *testing.T) {
+	z(t, []testcase{
+		{[]byte{0x34, 0x34, 0, 0, 0, 0}, 1, "44\\x00\\x00\\x00\\x00"},
+		{[]byte{0x34, 0x34, 0, 0, 0, 0, 0}, 1, "44\\x00\\x00\\x00\\x00\\x00"},
+		{[]byte{0x34, 0x34, 0, 0, 0, 0}, 2, "44\\x00\\x00\\x00\\x00"},
+		{[]byte{0x34, 0x34, 0, 0, 0, 0, 0}, 2, "44\\x00\\x00\\x00\\x00\\x00"},
+		{[]byte{0x34, 0x34, 0, 0, 0, 0}, 10, "\\x34\\x34\\x00\\x00\\x00\\x00"},
+		{[]byte{0x34, 0x34, 0, 0, 0, 0, 0}, 10, "\\x34\\x34\\x00\\x00\\x00\\x00\\x00"},
+	})
+}
+
+func TestEndReadable(t *testing.T) {
+	z(t, []testcase{
+		{[]byte{0, 0, 0, 0, 0x34, 0x34}, 1, "\\x00\\x00\\x00\\x0044"},
+		{[]byte{0, 0, 0, 0, 0, 0x34, 0x34}, 1, "\\x00\\x00\\x00\\x00\\x0044"},
+		{[]byte{0, 0, 0, 0, 0x34, 0x34}, 2, "\\x00\\x00\\x00\\x0044"},
+		{[]byte{0, 0, 0, 0, 0, 0x34, 0x34}, 2, "\\x00\\x00\\x00\\x00\\x0044"},
+		{[]byte{0, 0, 0, 0, 0x34, 0x34}, 10, "\\x00\\x00\\x00\\x00\\x34\\x34"},
+		{[]byte{0, 0, 0, 0, 0, 0x34, 0x34}, 10, "\\x00\\x00\\x00\\x00\\x00\\x34\\x34"},
+	})
+}
+
+func TestMidReadable(t *testing.T) {
+	z(t, []testcase{
+		{[]byte{0, 0, 0x34, 0x34, 0, 0}, 1, "\\x00\\x0044\\x00\\x00"},
+		{[]byte{0, 0, 0x34, 0x34, 0, 0, 0}, 1, "\\x00\\x0044\\x00\\x00\\x00"},
+		{[]byte{0, 0, 0x34, 0x34, 0, 0}, 2, "\\x00\\x0044\\x00\\x00"},
+		{[]byte{0, 0, 0x34, 0x34, 0, 0, 0}, 2, "\\x00\\x0044\\x00\\x00\\x00"},
+		{[]byte{0, 0, 0x34, 0x34, 0, 0}, 10, "\\x00\\x00\\x34\\x34\\x00\\x00"},
+		{[]byte{0, 0, 0x34, 0x34, 0, 0, 0}, 10, "\\x00\\x00\\x34\\x34\\x00\\x00\\x00"},
+	})
+}
+
+func TestEmpty(t *testing.T) {
+	z(t, []testcase{
+		{[]byte{}, 1, ""},
+		{[]byte{}, 2, ""},
+		{[]byte{}, 10, ""},
+	})
+}
+
+func TestInvalidM(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			return
+		}
+	}()
+	String([]byte{0, 0, 0, 0}, 0)
+	t.Error("String({}, 0) failed to panic")
+}
